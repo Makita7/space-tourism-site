0781488c59a594f364e41d6bd015c915de06d61f
Makita7 <sophiamakita@gamil.com>
2022-10-17 16:15:38 -0300

    installing sass

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,6 +4,21 @@
 
 <template>
   <main>
-    homeview
+    <p>
+      SO, YOU WANT TO TRAVEL TO
+    </p>
+    <h1>space</h1>
+    <p>
+      Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+    </p>
+    <button>
+        Explore
+    </button>
   </main>
 </template>
+
+<style lang="sass" scoped>
+  main
+    background-color: aqua
+
+</style>
