6a3534606557b892182fd58df5d8bed9e483d37e
Makita7 <sophiamakita@gamil.com>
2022-10-18 09:38:54 -0300

    homepage cellphone

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -12,9 +12,11 @@
       <p class="description">
         Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
       </p>
-      <button>
-          Explore
-      </button>
+      <RouterLink to="/destination">
+        <button>
+            Explore
+        </button>
+    </RouterLink>
     </div>
   </main>
 </template>
