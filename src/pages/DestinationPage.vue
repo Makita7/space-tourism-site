384cde659ec85e1bff22efe12ed5dc411adbaebc
Makita7 <sophiamakita@gmail.com>
2022-10-20 15:12:32 -0300

    destination cards

--- src/pages/DestinationPage.vue
+++ src/pages/DestinationPage.vue
@@ -1,7 +1,6 @@
 
 <script setup>
 
-
 const Destinations = [
     {
         id: 1,
@@ -24,16 +23,22 @@ const Destinations = [
         to: "/titan"
     }
 ]
+
 </script>
 
 
 <template>
     <main>
         <div class="destination">
-            <div class="wrapper">
+            <div class="wrapper centered">
+                <div class="flex centered">
+                    <p class="num">01</p>
+                    <p class="pick">Pick your destination</p>
+                </div>
                 <RouterLink class="link" v-for="planets in Destinations" :to="planets.to" :key="planets.id">{{planets.name}}</RouterLink><br/>
-                destination
-                <RouterView/>
+                <div class="centered">
+                    <RouterView/>
+                </div>
             </div>
         </div>
     </main>
@@ -51,8 +56,91 @@ const Destinations = [
     .wrapper{
         padding-top: 8rem;
         padding-left: 2rem;
+        padding-right: 2rem;
+    }
+    .num{
+        font-family: var(--BarlowC);
+        font-weight: 400;
+        font-size: 1.2rem;
+        color: rgba(255, 255, 255, 0.25);
+        margin-right: 1rem;
+        letter-spacing: 0.2rem;
+    }
+    .pick{
+        font-family: var(--BarlowC);
+        text-transform: uppercase;
+        font-size: 1.2rem;
+        font-weight: 300;
+        letter-spacing: 0.2rem;
     }
     .link{
         color: white;
+        padding: 0 1rem;
+        text-decoration: none;
+        font-family: var(--BarlowC);
+        font-size: 1rem;
+        text-transform: uppercase;
+        color: var(--inactiveColor);
+        font-weight: 300;
+        letter-spacing: 0.2rem;
+    }
+    .active-link{
+        color: white;
+        border-bottom: 3px solid rgb(255, 255, 255);
+        padding: 0 0.2rem 0.5rem 0.2rem;
+    }
+    :deep(.centered){
+        text-align: center !important;
+        justify-content: center;
+    }
+
+    :deep(.title){
+        font-size: 4rem;
+        text-transform: capitalize;
+        text-transform: uppercase;
+        font-family: var(--Bellfair);
+        font-weight: 500;
+        margin: 1.5rem 0rem -1rem 0rem;
+    }
+    :deep(.km){
+        font-family: var(--Bellfair);
+        font-size: 2rem;
+        text-transform: uppercase;
+        margin-top: 1rem;
+    }
+    :deep(.days){
+        font-family: var(--Bellfair);
+        font-size: 2rem;
+        text-transform: uppercase;
+        margin-top: 1rem;
+    }
+    :deep(.description){
+        font-family: var(--Barlow);
+        font-weight: 300;
+        line-height: 1.7rem;
+        margin-bottom: 2rem;
+        color: var(--inactiveColor);
+    }
+    :deep(hr){
+        border-color: #545768;
+        border-radius: 4px;
+    }
+    :deep(.distance){
+        font-weight: 500;
+        color: var(--inactiveColor);
+        font-family: var(--BarlowC);
+        font-size: 1rem;
+        letter-spacing: 0.2rem;
+        font-weight: 300;
+        text-transform: uppercase;
+    }
+    :deep(.travel){
+        font-weight: 500;
+        color: var(--inactiveColor);
+        font-family: var(--BarlowC);
+        font-size: 1rem;
+        letter-spacing: 0.2rem;
+        font-weight: 300;
+        text-transform: uppercase;
     }
 </style>
--- src/pages/destinations/DestinationMoon.vue
+++ src/pages/destinations/DestinationMoon.vue
@@ -1,3 +1,15 @@
 <template>
-    Moon
+    <h4 class="title">Moon</h4>
+    <p class="description">
+        See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+    </p>
+    <hr/>
+    <p class="distance">AVG. DISTANCE</p>
+    <p class="km">384,400 km</p>
+    <p class="travel">Est. travel time</p>
+    <p class="days">3 days</p>
 </template>
+
+<style scoped>
+
+</style>
