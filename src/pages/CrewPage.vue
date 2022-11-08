9c01e1d9358942982be06ceff57a74340a8f4da3
Makita7 <sophiamakita@gmail.com>
2022-11-08 16:09:20 -0300

    crew animations

--- src/pages/CrewPage.vue
+++ src/pages/CrewPage.vue
@@ -1,42 +1,4 @@
 <script setup>
-    import Commander from '../assets/crew/image-douglas-hurley.png';
-    import Pilot from '../assets/crew/image-victor-glover.png';
-    import MisionSpecialist from '../assets/crew/image-mark-shuttleworth.png';
-    import FlightEngineer from '../assets/crew/image-anousheh-ansari.png';
-import TheCommander from './crew/TheCommander.vue';
-
-    const Slides = [
-        {
-            id: 1 ,
-            name: "Douglas Hurley",
-            position: "Commander",
-            img: Commander,
-            description: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.'
-        },
-        {
-            id: 2 ,
-            name: "MARK SHUTTLEWORTH",
-            position: "Mission Specialist ",
-            img: MisionSpecialist,
-            description: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
-        },
-        {
-            id: 3 ,
-            name: "Victor Glover",
-            position: "PILOT",
-            img: Pilot,
-            description: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. '
-        },
-        {
-            id: 4 ,
-            name: "Anousheh Ansari",
-            position: "Flight Engineer",
-            img: FlightEngineer,
-            description: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. '
-        },
-    ];
-
-
 
 </script>
 
@@ -48,7 +10,11 @@ import TheCommander from './crew/TheCommander.vue';
                 <p class="pick">Meet your crew</p>
             </div>
             <div class="slides">
-                <RouterView :key="$route.fullPath" />
+                <router-view  v-slot="{ Component }" >
+                    <transition name="route" mode="out-in">
+                        <component :is="Component"/>
+                    </transition>
+                </router-view>
             </div>
         </div>
     </div>
@@ -103,5 +69,23 @@ import TheCommander from './crew/TheCommander.vue';
         font-family: var(--Bellfair);
         font-weight: 200;
         font-size: 1.3rem;
+    }
+
+    /* ANIMATIONS */
+    .route-enter-from{
+        opacity: 0;
+        transform: translateX(100px);
+    }
+
+    .route-enter-active{
+        transition: all 0.3s ease-out;
+    }
+    .route-leave-active{
+        opacity: 0;
+        transform: translateX(-100px);
+    }
+
+    .route-leave-active{
+        transition: all 0.3s ease-in;
     }
 </style>
