298a08c558093d9db4a0f633a41fb8695010d430
Makita7 <sophiamakita@gmail.com>
2022-11-10 07:20:51 -0300

    button animation

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -13,7 +13,7 @@
         Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
       </p>
       <RouterLink to="/moon">
-        <button>
+        <button class="AFadeIn">
             Explore
         </button>
     </RouterLink>
