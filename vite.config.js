99216390da058b79325e23626318e9b3e576df3e
Makita7 <sophiamakita@gmail.com>
2022-11-12 09:34:58 -0300

    base configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/space-tourism-site/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
